1956626a5ca3f54cb683160cfb0305908fef6068
sakshi-ladkat <ladkatsakshi2507@gmail.com>
2024-07-27 23:34:53 +0530

    Create p2.c

---
+++ p2.c
@@ -0,0 +1,23 @@
+//Wtite a code to make a pattern 
+222
+212
+222
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    for(i=0;i<3;i++)
+    {
+    
+    for(j=0;j<3;j++)
+    {
+        if(i==1 && j==1)
+        printf("1\t");
+        else
+        printf("2\t");
+    }
+    printf("\n");
+    }
+}
