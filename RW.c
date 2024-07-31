aaa6396de2a25f1875ae0a9b84b0b906403abb81
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2024-07-31 23:41:10 +0530

    Create RW.c

---
+++ RW.c
@@ -0,0 +1,25 @@
+// Hello World
+#include<stdio.h>
+int main()
+{
+  printf("Hello world\n");
+return 0;
+}
+
+//Go To Hell
+#include<stdio.h>
+int main()
+{
+  printf("Go To Hell\n");
+return 0;
+}
+
+//Declaring Variable 
+#include<stdio.h>
+int main()
+{
+  int i=10;
+  float j=10.1;
+  printf("value of i=\t%d \n value of j=\t%f",i,j);
+  return 0;
+}
