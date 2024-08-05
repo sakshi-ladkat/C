baf8eb306be59823047a197b6e04db955d29c1bc
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2024-08-05 23:15:21 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -77,3 +77,16 @@ else
 printf("%d is negative");
 }
 }
+
+//Program To write For the quadratic equation 
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int a=6,b=-17,c=12,x,y;
+   printf("Quadratic equation %dx^2%dx+%d\n\n",a,b,c);
+   x=-b-(sqrt(b*b-4*a*c))/2*a;
+   y=-b+(sqrt(b*b-4*a*c))/2*a;
+printf("Roots Of Quadratic equations are \n\n%d \t %d",x,y);
+return 0;
+}
