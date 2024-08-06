9fa740c4c32705e8b6e4f45c6158dfd195771ec5
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2024-08-07 00:01:15 +0530

    Update RW.c

--- RW.c
+++ RW.c
@@ -23,3 +23,27 @@ int main()
   printf("value of i=\t%d \n value of j=\t%f",i,j);
   return 0;
 }
+
+//Swap two numbers using third variable
+#include<stdio.h>
+int main()
+{
+int a=5;
+int b=10;
+int c;
+c=b;
+b=a;
+a=c;
+printf("a=%d\tb=%d\tc=%d\n",a,b,c);
+return 0;
+}
+
+//Quadratic equation program
+#include<stdio.h>
+int main()
+{
+  int x=3;
+int y=(x*x*x)+(x*x)+x;
+printf("x=%d and y=%d",x,y);
+return 0;
+}
