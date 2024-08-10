cdd0bd728f22ab10d07fb22e9a4693acd3b4e29b
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2024-08-10 23:25:23 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -45,3 +45,91 @@ int main()
 printf("Roots Of Quadratic equations are \n\n%d \t %d",x,y);
 return 0;
 }
+
+
+// Greater & Smaller
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a & b :");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        printf("%d is Greater than %d",a,b);
+    }
+    else if(b>a)
+    {
+        printf("%d is Greater than %d",b,a);
+    }
+    else
+    {
+        printf("%d is equal to %d",a,b);
+    }
+    return 0;
+}
+
+//Division 
+#include <stdio.h>
+int main()
+{
+    int a,b,rem,quo;
+    printf("Enter Value of a & b :");
+    scanf("%d %d",&a,&b);
+    if(b!=0)
+    {
+        rem=a%b;
+        quo=a/b;
+        printf("Remainder =\t%d\n Quotient=\t%d",rem,quo);
+    }
+    else if(b==0)
+    {
+        printf("Divide By Zero Error");
+    }
+    return 0;
+}
+
+// less than 10 print small number greater than 10 print large Number
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter an integer :");
+    scanf("%d",&a);
+    if(a<10)
+    {
+        printf("Small Number");
+    }
+    else if(a>10)
+    {
+        printf("Large Number");
+    }
+    else
+    {
+        printf("\n %d is equal to 10");
+    }
+    return 0;
+}
+
+//less than 100 print small number greater than 100 print large number and less than 1000 print Big Number , 
+if it is greater than 1000 print large number
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter an integer :");
+    scanf("%d",&a);
+    if(a<100)
+    {
+        printf("Small Number");
+    }
+    else if(a>100 && a<1000)
+    {
+        printf("Big Number");
+    }
+    else
+    {
+        printf("Large Number");
+    }
+    return 0;
+}
