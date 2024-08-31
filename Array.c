67d791f17563101b11a623f46f55c487be40473d
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2024-08-31 14:46:43 +0530

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,18 @@
+Read five integers from the user and print them.
+2. Read five integers from the user and print them using an integer array.
+3. Read five floating-point numbers from the user and print them into a float
+array.
+4. Read five characters from the user and print them using a character array.
+5. Read five integers from the user add them and print the addition.
+6. Read five integers from the user and add them if the addition is greater than
+10 divide the addition by 2 else divide the addition by 3 and print the answer.
+7. Create an integer array of size 5 read 10 integers into this array and print the
+integers. (Make a comment on the output.)
+8. Read a character from the user and print it.
+9. Read a string from the user and print it.
+10. Read a string with spaces from the user and print it (e.g. “Hello World”).
+211. Read a 2x2 matrix from the user and print it
+12. Read a 3x3 matrix from the user and print it
+13. Read a 2x2 matrix from the user and print the addition of each row
+14. Read a 3x3 matrix from the user and print the addition of each row and each
+column
