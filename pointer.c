f04e947ca0c8e65b9cfc4d6e938303fabd1c6655
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2024-09-05 23:42:32 +0530

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -1,10 +1,11 @@
-//1.c
 #include<stdio.h>
 int main()
 {
     int i=10;
     int *p=&i;
+    printf("value=%d\n",i);
+    printf("Address=%d\n",&i);
+    printf("Value =%d\n",*p);
     printf("Adress =%d\n",p);
-     printf("Value =%d\n",*p);
 
 }
