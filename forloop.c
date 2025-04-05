f5afa9d047f039f5b38480bfbcb733e4e80cd9f3
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2025-04-05 20:43:38 +0530

    Update forloop.c

--- forloop.c
+++ forloop.c
@@ -202,3 +202,18 @@ int main()
     }
     printf("sum=%d\tmult=%d",sum,mult);
 }
+//13.c Read five integers from the user and perform their addition, and multiplication user for loop
+#include<stdio.h>
+int main()
+{
+    int n=5,i,sum=0,mult=1;
+    for(i=0;i<5;i++)
+    {
+        printf("Enter Element");
+        scanf("%d",&n);
+        sum +=n;
+        mult=mult*n;
+        
+    }
+    printf("sum=%d\tmult=%d",sum,mult);
+}
